f1cbe191f16654fc2a5468a53c755f747592c422
Minh Le <lenguyetminh1998@gmail.com>
2017-12-27 16:57:44 -0500

    IN PROGRESS: interest page

--- www/js/controllers/interest.js
+++ www/js/controllers/interest.js
@@ -1,3 +1,24 @@
 app.controller('InterestCtrl', function($scope, Chats) {
+  $scope.search = 1;
+  $scope.DisplayData = [];
 
+  //------- FUNCTION TO ADD AN INTEREST ---------
+  $scope.AddInterest = function(){
+    $scope.search = 1;
+  };
+
+  //------- CALL THE INTEREST DATA FROM FIREBASE --------
+  var interestRef = firebase.database().ref("interests");
+  interestRef.on("value", function(snapshot){
+    $scope.InterestData = snapshot.val();
+    $scope.interest = {};
+    for (var key in $scope.InterestData){
+      $scope.interest.name = key;
+      $scope.interest.likes = $scope.InterestData[key];
+      $scope.DisplayData.push($scope.interest);
+      $scope.interest = {};
+    }
+    console.log($scope.DisplayData);
+    $scope.$apply();
+  });
 });
