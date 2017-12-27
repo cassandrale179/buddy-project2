c3a0da14bd464ad57e5d59c12747a769bc6bcee9
Minh Le <lenguyetminh1998@gmail.com>
2017-12-27 11:21:53 -0500

    DONE: user age, name and description

--- www/js/controllers/profile.js
+++ www/js/controllers/profile.js
@@ -1,24 +1,22 @@
 app.controller('ProfileCtrl',  ['$scope', '$state',
 function($scope) {
-  // $scope.name = "Elliot Anderson";
+  var user = firebase.auth().currentUser;
+  var dateObj = (new Date()).getFullYear();
   $scope.$on('$ionicView.enter', function() {
-    var userRef = firebase.database().ref("users/" + "S9TZy3VPmSTv8Y7dnicGIdkXRkE3");
-    userRef.on("value", function(snapshot){
-      $scope.name = snapshot.val().name; 
-      $scope.$apply();
-      //alert($scope.name);
-    });
+    //if (user !== null){
+    //  var userRef = firebase.database().ref("users/" + user.uid);
+    var userRef = firebase.database().ref("users/" + "aUxZ8uPNgbc4USL4gTr1CXBiHHN2");
+      userRef.on("value", function(snapshot){
+        $scope.name = snapshot.val().name;
+        $scope.description = snapshot.val().description;
+        $scope.birthyear = parseInt(snapshot.val().birthyear.substring(0,4));
+        $scope.age = dateObj - $scope.birthyear;
+        $scope.$apply();
+      });
+  //  }
+    // else{
+    //   console.log("Please login again");
+    // }
   });
-//   var user = firebase.auth().currentUser;
-//   //if (user !== null){
-//   //  var userRef = firebase.database().ref("users/" + user.uid);
-//     var userRef = firebase.database().ref("users/" + "S9TZy3VPmSTv8Y7dnicGIdkXRkE3");
-//     userRef.on("value", function(snapshot){
-//       userData = snapshot.val();
-//       $scope.name = snapshot.val().name;
-//       console.log($scope.name);
-//       $scope.age = snapshot.val().age;
-//     });
-// //  }
 
 }]);
--- www/js/controllers/settings.js
+++ www/js/controllers/settings.js
@@ -1,5 +1,13 @@
-app.controller('SettingsCtrl', function($scope) {
+app.controller('SettingsCtrl', function($scope, $state) {
   $scope.settings = {
     enableFriends: true
+  };
+
+  $scope.LogOut = function(){
+    var auth = firebase.auth();
+    auth.signOut().then(function(){
+      console.log("Logged out!");
+      $state.go('register');
+    });
   };
 });
