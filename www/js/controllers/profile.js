d08851f5c2a3af8da3a68aabdb6ccf466fa21b83
Minh Le <lenguyetminh1998@gmail.com>
2017-12-28 21:30:12 -0500

    DONE: ngCordova installed

--- www/js/controllers/profile.js
+++ www/js/controllers/profile.js
@@ -2,23 +2,23 @@ app.controller('ProfileCtrl',  ['$scope', '$state',
 function($scope) {
   var user = firebase.auth().currentUser;
   var dateObj = (new Date()).getFullYear();
-  $scope.$on('$ionicView.enter', function() {
-    //if (user !== null){
-    //  var userRef = firebase.database().ref("users/" + user.uid);
-    var userRef = firebase.database().ref("prod/users/" + "HU3IqF8KodfYPY9yKMPuEHYC2A53");
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user){
+      var userRef = firebase.database().ref("prod/users/" + user.uid);
       userRef.on("value", function(snapshot){
         $scope.name = snapshot.val().name;
         $scope.description = snapshot.val().description;
         $scope.birthyear = parseInt(snapshot.val().birthyear.substring(0,4));
         $scope.age = dateObj - $scope.birthyear;
         $scope.pictureUrl = snapshot.val().pictureUrl;
-        $scope.interests = snapshot.val().interest.split(",").length - 1; 
+        $scope.interests = snapshot.val().interest.split(",").length - 1;
         $scope.$apply();
-      });
-  //  }
-    // else{
-    //   console.log("Please login again");
-    // }
+       });
+    }
+    else{
+       console.log("Please login again");
+    }
+
   });
 
 }]);
--- www/js/controllers/settings.js
+++ www/js/controllers/settings.js
@@ -1,13 +1,50 @@
-app.controller('SettingsCtrl', function($scope, $state) {
-  $scope.settings = {
-    enableFriends: true
-  };
+app.controller('SettingsCtrl', function($scope, $state, $cordovaCamera) {
+  var user = firebase.auth().currentUser;
+  firebase.auth().onAuthStateChanged(function(user) {
 
-  $scope.LogOut = function(){
-    var auth = firebase.auth();
-    auth.signOut().then(function(){
-      console.log("Logged out!");
-      $state.go('register');
-    });
-  };
+      //----------------- IF USER IS LOGGED IN ---------------
+      if (user){
+
+        //----------- FUNCTION TO UPLOAD PICTURE ------------
+        $scope.UploadPicture = function(){
+          document.addEventListener("deviceready", function () {
+            var options = {
+              quality: 50,
+              destinationType: Camera.DestinationType.DATA_URL,
+              sourceType: Camera.PictureSourceType.CAMERA,
+              allowEdit: true,
+              encodingType: Camera.EncodingType.JPEG,
+              targetWidth: 100,
+              targetHeight: 100,
+              popoverOptions: CameraPopoverOptions,
+              saveToPhotoAlbum: false,
+        	     correctOrientation:true
+            };
+          });
+        };
+
+
+        //--------- FUNCTION TO LOG OUT USER -------------
+        $scope.LogOut = function(){
+          var auth = firebase.auth();
+          auth.signOut().then(function(){
+            console.log("Logged out!");
+            $state.go('login');
+          });
+        };
+      }
+
+
+      //---------- IF USER IS NOT LOGGED IN, REDIRECT TO LOGIN PAGE ----------
+      else{
+        $state.go('login');
+      }
+  });
+
+
+
+
 });
+// $scope.settings = {
+//   enableFriends: true
+// };
