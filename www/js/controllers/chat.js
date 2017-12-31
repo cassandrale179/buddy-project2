305ae2d6f6fd25e038aeef461633bee145d51578
Minh Le <lenguyetminh1998@gmail.com>
2017-12-31 13:07:09 -0500

    DONE: correctly pass the convoid as stateparams

--- www/js/controllers/chat.js
+++ www/js/controllers/chat.js
@@ -1,10 +1,9 @@
-app.controller('ChatsCtrl', function($scope, Message, $firebaseAuth) {
+app.controller('ChatsCtrl', function($scope, $firebaseAuth) {
   $firebaseAuth().$onAuthStateChanged(function(user) {
     var refUser = firebase.database().ref("prod/users/" + user.uid + "/buddies");
     refUser.on("value", function(snapshot){
       var buddiesList = snapshot.val();
       $scope.chats = Object.values(buddiesList);
-      console.log($scope.chats);
     });
   });
 
