4d16e0999e5bfc4aaf9147f14dfb9b5df616badf
Cassandra Le <lenguyetminh1998@gmail.com>
2018-03-17 11:21:59 -0400

    IN PROGRESS: nested promises warning from linter

--- functions/firebase-messaging-sw.js
+++ functions/firebase-messaging-sw.js
@@ -13,16 +13,16 @@ var config = {
 };
 firebase.initializeApp(config);
 const messaging = firebase.messaging();
-//
-// messaging.setBackgroundMessageHandler(function(payload) {
-//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//     // Customize notification here
-//     const notificationTitle = 'Background Message Title';
-//     const notificationOptions = {
-//         body: 'Background Message body.',
-//         icon: '/firebase-logo.png'
-//     };
-//
-// return self.registration.showNotification(notificationTitle,
-//     notificationOptions);
-// });
+
+messaging.setBackgroundMessageHandler(payload => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
